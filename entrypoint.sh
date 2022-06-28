799771797c44eef7f4bd3193c6e1a710a1ccfe9f
Fat Rat <60702632+liquid8796@users.noreply.github.com>
2022-06-29 06:50:26 +0700

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-cd /minio
-wget https://dl.minio.io/server/minio/release/linux-amd64/minio --no-check-certificate
-chmod +x minio
-export MINIO_ACCESS_KEY="$ACCESS_KEY"
-export MINIO_SECRET_KEY="$SECRET_KEY"
-./minio server --address :$PORT /mnt/minio/data
